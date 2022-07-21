1838b853d0dbd8317c7db4ea0a8442a959d4fc87
Noah Kurrack <noahkurrack@gmail.com>
2022-07-20 22:38:50 -0400

    update user pool attributes and signup hook

--- cognito/lambda-triggers/pre-sign-up/pre-sign-up.ts
+++ cognito/lambda-triggers/pre-sign-up/pre-sign-up.ts
@@ -5,5 +5,6 @@ import { PreSignUpTriggerHandler } from 'aws-lambda';
 
 export const handler: PreSignUpTriggerHandler = async event => {
     event.response.autoConfirmUser = true;
+    event.response.autoVerifyEmail = true;
     return event;
 };

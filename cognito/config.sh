c6807a9b9f0ae98fdff30ec26c7b220e90fcca18
Noah Kurrack <noahkurrack@gmail.com>
2022-07-20 23:08:15 -0400

    add environment variables to config file

---
+++ cognito/config.sh
@@ -0,0 +1,4 @@
+export S3_BUCKET_NAME=codereal-deployments
+export SES_FROM_ADDRESS=no-reply@codereal.app
+export STACK_NAME=serverlessrepo-codereal-email-auth
+export USER_POOL_NAME=CodeRealUserPool

152e9b49d5510050f2837cd67e179909528f8049
Noah Kurrack <noahkurrack@gmail.com>
2022-07-24 16:33:48 -0400

    cleanup

--- cognito/config.sh
+++
@@ -1,4 +0,0 @@
-export S3_BUCKET_NAME=codereal-deployments
-export SES_FROM_ADDRESS=no-reply@codereal.app
-export STACK_NAME=serverlessrepo-codereal-email-auth
-export USER_POOL_NAME=CodeRealUserPool
